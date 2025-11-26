2a10496389b2f451fe63776e6d80d5b2f0200109
MUTHUGOPI J <massg754@gmail.com>
2025-11-26 22:32:50 +0530

    Delete routes/visitor.routes.mjs

--- routes/visitor.routes.mjs
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { trackVisitor, getVisitors } from '../controllers/visitor.conntrollers.mjs';
-
-const router = express.Router();
-
-router.post("/visitor", trackVisitor);
-router.get('/visitors', getVisitors);
-
-export default router
