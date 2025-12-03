23020f5e99776f48ddcf96107e32aa22e1867e5d
MUTHUGOPI J <massg754@gmail.com>
2025-12-03 17:10:32 +0530

    Add files via upload

--- controllers/auth.controller.mjs
+++ controllers/auth.controller.mjs
@@ -1,16 +1,15 @@
 import db from "../src/utils/db.mjs";
 import { customeError, serverError, notFound } from "../src/utils/errorHandling.mjs";
+import bcrypt from 'bcrypt';
 
 export const checkUser = (req, res, next) => {
     const { name, password } = req.body;
 
     const query = 'SELECT * FROM users WHERE name = ?';
-    db.query(query, [name], (err, data) => {
 
-        if (err) {
-            return serverError(res);
-        }
+    db.query(query, [name], async (err, data) => {
 
+        if (err) return serverError(res);
 
         if (data.length === 0) {
             return notFound(res, "User not found");
@@ -18,25 +17,31 @@ export const checkUser = (req, res, next) => {
 
         const user = data[0];
 
-        if (user.password !== password) {
-            return customeError(res, 400, `Password doen't matchedm : ${data}`);
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return customeError(res, 400, "Password does not match");
         }
-        next()
+
+        next();
     });
 };
 
 
-export const isAdmin = (req, res) => {
-    const {name} = req.body;
+export const isAdmin = (req, res, next) => {
+    const { name } = req.body;
     const query = 'SELECT * FROM admins WHERE name = ?';
-    db.query(query, name, (err, res) => {
-        if(err) {
-            return serverError(res);
+    
+    db.query(query, [name], (err, result) => {
+        if (err) {
+            return serverError(res); 
+        } 
+        if (result.length === 0) {
+            return notFound(res, "You're not an admin buddy!!");
         }
-        else if(data.length == 0) {
-            return notFound(res, "Your Not A Admin Buddy !!");
-        }
-        
-    })
+        console.log("access garented")
+        next();
+    });
 }
+
 
--- routes/users.routes.mjs
+++ routes/users.routes.mjs
@@ -2,10 +2,11 @@ import express from 'express';
 import {getAllUsers, createUser} from '../controllers/users.controller.mjs'
 import { checkSchema } from 'express-validator';
 import { createUserValidationSchema } from '../src/utils/validationSchemas.mjs';
+import { isAdmin } from '../controllers/auth.controller.mjs';
 
 const router = express.Router();
 
-router.get('/', getAllUsers);
+router.get('/', isAdmin, getAllUsers);
 router.post('/', checkSchema(createUserValidationSchema) ,createUser);
 
 export default router
